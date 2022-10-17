d71cbdc9034ab4855d6372fa5ec10a121bdc0b82
Subzy132 <subhaanh00@gmail.com>
2022-10-17 09:20:04 +0100

    worked on odd even counter

---
+++ odd_even_counter.py
@@ -0,0 +1,15 @@
+
+def odd_even_counter(number):
+    num_list = [0,0]
+
+    while number != 0:
+        if number % 2 = 0:
+            num_list[0] = num_list[0] + number
+        else:
+            num_list[1] = num_list[1] + number
+
+        number = number - 1
+    return num_list
+
+number = int(input("Input a number:"))
+print(odd_even_counter(number))
--- password.py
+++ password.py
@@ -1,7 +1,13 @@
-password = "password123"
+password = "passworddasfdsdgagfsasf123"
 
 def length_check(password):
     if len(password) < 5:
         print("Your password is too short")
     elif len(password) > 5 and len(password) < 21:
         print("Your password is an acceptable length.")
+    elif len(password) > 20:
+        print("Your password is way too long and may be forgotten")
+    else:
+        print("something went wrong")
+
+length_check(password)
